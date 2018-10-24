9876494d09b24bcd8cb70f226b827f42a060cb79
Anton Melnyk <an.melnik@gmail.com>
2018-10-24 16:53:16 +0300

    RunScp works

--- My_shell/src/RunScp.bat
+++ My_shell/src/RunScp.bat
@@ -1,4 +1,6 @@
-C:\Windows\System32\OpenSSH\ssh.exe amelnyk@192.168.56.101 "/sbin/ifconfig enp0s3"
 C:\Windows\System32\OpenSSH\ssh.exe amelnyk@192.168.56.101 "rm -rf /home/amelnyk/Documents/Avid/techTask/*"
 C:\Windows\System32\OpenSSH\scp.exe C:\Users\anton.melnyk\Documents\workspace\MyPets\My_shell\src\cleadDir.sh amelnyk@192.168.56.101:/home/amelnyk/Documents/Avid/techTask/
+C:\Windows\System32\OpenSSH\scp.exe -r C:\Users\anton.melnyk\Documents\Avid\TechTask\mdbackup amelnyk@192.168.56.101:/home/amelnyk/Documents/Avid/techTask/
 C:\Windows\System32\OpenSSH\ssh.exe amelnyk@192.168.56.101 "chmod u+x /home/amelnyk/Documents/Avid/techTask/cleadDir.sh"
+C:\Windows\System32\OpenSSH\ssh.exe amelnyk@192.168.56.101 "/home/amelnyk/Documents/Avid/techTask/cleadDir.sh /home/amelnyk/Documents/Avid/techTask/mdbackup"
+C:\Windows\System32\OpenSSH\ssh.exe amelnyk@192.168.56.101 "ls -la /home/amelnyk/Documents/Avid/techTask/mdbackup"
