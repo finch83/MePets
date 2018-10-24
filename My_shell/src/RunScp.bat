27dca26684943f6aacf78651d803b2b9bb209ba6
Anton Melnyk <an.melnik@gmail.com>
2018-10-24 16:39:31 +0300

    Initial commit for RunScp.bat

---
+++ My_shell/src/RunScp.bat
@@ -0,0 +1,4 @@
+C:\Windows\System32\OpenSSH\ssh.exe amelnyk@192.168.56.101 "/sbin/ifconfig enp0s3"
+C:\Windows\System32\OpenSSH\ssh.exe amelnyk@192.168.56.101 "rm -rf /home/amelnyk/Documents/Avid/techTask/*"
+C:\Windows\System32\OpenSSH\scp.exe C:\Users\anton.melnyk\Documents\workspace\MyPets\My_shell\src\cleadDir.sh amelnyk@192.168.56.101:/home/amelnyk/Documents/Avid/techTask/
+C:\Windows\System32\OpenSSH\ssh.exe amelnyk@192.168.56.101 "chmod u+x /home/amelnyk/Documents/Avid/techTask/cleadDir.sh"
