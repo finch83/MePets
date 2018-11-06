ada32fd247368836b2298569ca63eab1e9f4844c
Anton Melnyk <an.melnik@gmail.com>
2018-11-06 16:55:42 +0200

    Updated with functions

--- My_shell/src/cleadDir.sh
+++ My_shell/src/cleadDir.sh
@@ -1,91 +1,102 @@
 #!/bin/bash
 
-if [ "$1" != "" ]; then
-    if [ -d "$1" ]; then
-        cd "$1"
-        tmpDir=`pwd`
-        if [ -z "$(ls -A $tmpDir)" ]; then
-            echo "Empty Dir"
-            exit 1
-        fi
-    else
-        echo "Dir not found"
-        exit 1 
-    fi
-else
-    echo "No cli arguments"
-    exit 1
-fi
 
-SRCDIR=$tmpDir
-echo "Source Dir: $SRCDIR"
-
-regexp="^Partition[0-1]{1}\.[0-9]{8}T[0-9]{6}\.verified\.gz$"
-
+regexp=".*/Partition[0-1]{1}\.[0-9]{8}T[0-9]{6}\.verified\.gz$"
 FILEDATE=""
 FILE1=""
 FILEDATE1=0
 FILE2=""
 FILEDATE2=0
+SRCDIR=""
+REMOVEDFILES=()
+REMOVEDIRS=()
+# ToDo Add check for second paramenter
+LEFTFILES=$2
+declare -A MAPFILEDATE
 
-rm -f "$SRCDIR"/.*
 
-for file in "$SRCDIR"/*
-do
-#    echo "$file"
-    tmpFile=`basename "$file"`
-
-    if [ -d "$file" ]; then
-        rm -rf $file
-        continue
-    fi
-
-    if [[ "$tmpFile" =~ $regexp ]]; then 
-        FILEDATE=`echo $tmpFile | awk -F'.' '{print $2}' | awk -F'T' '{print $1$2}'`
-        if [ "$FILE1" == "" ]; then
-            FILE1=$file
-            FILEDATE1=$FILEDATE
-            continue
-        elif [ "$FILE2" == "" ]; then
-            FILE2=$file
-            FILEDATE2=$FILEDATE
-            if [ $FILEDATE -gt $FILEDATE1 ]; then
-                FILEDATE2=$FILEDATE1
-                FILE2=$FILE1
-                FILEDATE1=$FILEDATE
-                FILE1=$file
-                continue
-           fi
-        fi
-
-        if [ $FILEDATE -gt $FILEDATE2 ]; then
-            if [ $FILEDATE -gt $FILEDATE1 ]; then
-                echo "Remove File: $FILE2"
-                rm $FILE2
-                FILE2=$FILE1
-                FILEDATE2=$FILEDATE1
-                FILE1=$file
-                FILEDATE1=$FILEDATE
-            else
-               FILE2=$file
-               FILEDATE2=$FILEDATE
+#------------
+check_args()
+{
+    if [ "$1" != "" ]; then
+        if [ -d "$1" ]; then
+            cd "$1"
+            tmpDir=`pwd`
+            if [ -z "$(ls -A $tmpDir)" ]; then
+                echo "Empty Dir"
+                exit 1
             fi
         else
-            echo "Remove File: $file"
-            rm $file
+            echo "Dir not found"
+            exit 1 
         fi
     else
-        echo "Remove File: $file"
-        rm $file 
-   fi
-   echo
+        echo "No cli arguments"
+        exit 1
+    fi
+#    echo $tmpDir
+    eval "$2=$tmpDir"
+}
 
-done
+
+#------------
+remove_file()
+{
+    if [ -d $1 ]; then
+        REMOVEDIRS+=($1)
+    else
+        REMOVEDFILES+=($1)
+    fi
+}
 
 
-echo
-echo "File1: $FILE1"
-echo "File1Date: $FILEDATE1"
-echo "File2: $FILE2"
-echo "File2Date: $FILEDATE2"
+#------------
+check_args $1 SRCDIR
 
+main()
+{
+    find $SRCDIR -regextype egrep ! -regex $regexp -delete
+
+    for file in "$SRCDIR"/*
+    do
+        tmpFile=`basename "$file"`
+
+        if [ -d "$file" ]; then
+            remove_file $file
+            continue
+        fi
+    
+        FILEDATE=`echo $tmpFile | awk -F'.' '{print $2}' | awk -F'T' '{print $1$2}'`
+        MAPFILEDATE[$file]=$FILEDATE
+
+    done
+
+    sortedString=`for key in ${!MAPFILEDATE[@]};
+    do
+        echo ${key} ${MAPFILEDATE[${key}]}
+    done | sort -k2rn | awk '{ print $1}'`
+
+    sortedArray=($(echo $sortedString | tr "\n" " "))
+    for (( i = $LEFTFILES; i < ${#sortedArray[@]}; i++ )); do
+        remove_file ${sortedArray[i]}
+    done
+    
+    echo "*----------Removed Dirs----------*"
+    for (( i = 0; i < ${#REMOVEDIRS[@]}; i++ )); do
+        rm -rf ${REMOVEDIRS[i]}
+        echo "${REMOVEDIRS[i]}"
+    done
+    
+    echo "*----------Removed Files----------*"
+    for (( i = 0; i < ${#REMOVEDFILES[@]}; i++ )); do
+        rm -f ${REMOVEDFILES[i]}
+        echo "${REMOVEDFILES[i]}"
+    done
+    
+    echo "*----------Left files----------*"
+    for (( i = 0; i < $LEFTFILES; i++ )); do
+        echo ${sortedArray[i]}
+    done
+}
+
+main
